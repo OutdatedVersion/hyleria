5278dfa93e532b7672122e3bde50a8eb06accabe
Ben (OutdatedVersion) <outdated@outdatedversion.com>
2017-04-30 11:10:54 -0500

    move things to a more accessible spot

--- web-api/data.js
+++ web-api/data.js
@@ -15,26 +15,19 @@ let grabPlayer = (key) =>
     // we may use either a user's UUID or name to look
     // them up so we need to validate that we are using
     // one or the other
-    let providedUUID = key.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    let providedUUID = util.isUUID(key)
 
-    if (!providedUUID && !key.match(/[a-zA-Z0-9_]{1,16}$/))
+    if (!providedUUID && !util.isValidUsername(key))
         return { err: 'invalid username provided' }
 
 
-    // api:p:outdatedversion
     // api:p:03c337cd7be04694b9b0e2fd03f57258
 }
 
-/**
- *
- * @param key
- * @param isUUID
- * @param collection
- * @param callback
- */
+
 let fetchFromMongo = (key, isUUID, collection, callback) =>
 {
-    collection.find({})
+
 }
 
 
--- web-api/util.js
+++ web-api/util.js
@@ -31,8 +31,38 @@ let dash = (val) =>
     return val.substr(0, 8) + '-' + val.substr(8, 11) + '-' + val.substr(11, 18) + '-' + val.substr(19, 23) + '-' + val.substr(24, 36)
 }
 
+/**
+ * Checks whether the provided String
+ * is a valid UUID
+ *
+ * @param val the string
+ * @returns {boolean} yes or no
+ */
+let isUUID = (val) =>
+{
+    assert(typeof val === 'string', 'we can only verify a string')
+
+    return val.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+}
+
+/**
+ * Checks whether the provided String
+ * is a valid Minecraft username
+ *
+ * @param val the string
+ * @returns {boolean} yes or no
+ */
+let isValidUsername = (val) =>
+{
+    assert(typeof val === 'string', 'we can only verify a string')
+
+    return val.match(/[a-zA-Z0-9_]{1,16}$/)
+}
+
 
 export default {
     undash,
-    dash
+    dash,
+    isUUID,
+    isValidUsername
 }
