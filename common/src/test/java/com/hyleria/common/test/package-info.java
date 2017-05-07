183b7978d0d9f9d41fe34062278ae8fbb762b18d
Ben (OutdatedVersion) <outdated@outdatedversion.com>
2017-05-06 22:15:00 -0500

    some more package info stuff & regen the docs

---
+++ common/src/test/java/com/hyleria/common/test/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Tests for our {@code common} module.
+ *
+ * @since May/06/2017 (10:12 PM)
+ */
+package com.hyleria.common.test;
---
+++ plugin/lobby/src/main/java/com/hyleria/lobby/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Anything relating to our base
+ * player staging servers.
+ *
+ * @since May/06/2017 (10:13 PM)
+ */
+package com.hyleria.lobby;
